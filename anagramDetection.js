b0efb7f4a6286c3f87fb51a14a97a1683dd8f5f7
Charles Willcockson <willcockson.charles@gmail.com>
2022-01-15 14:38:11 -0600

    added anagram detection

---
+++ anagramDetection.js
@@ -0,0 +1,22 @@
+// An anagram is the result of rearranging the letters of a word to produce a new word (see wikipedia).
+//
+// Note: anagrams are case insensitive
+//
+// Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+//
+//     Examples
+// "foefet" is an anagram of "toffee"
+//
+// "Buckethead" is an anagram of "DeathCubeK"
+
+var isAnagram = function(test, original) {
+    let tester = test.toLowerCase().split('').sort().join('');
+    console.log(tester);
+    let originaler = original.toLowerCase().split('').sort().join('');
+    console.log(originaler);
+    if (tester === originaler){
+        return true;
+    }else return false;
+};
+
+console.log(isAnagram('Buckethead', 'DeathCubek'));
