c681b32a659be374456e2fcc5e88e14ac0bcfa48
Charles Willcockson <willcockson.charles@gmail.com>
2022-01-23 23:09:17 -0600

    Century From Year kata

---
+++ centuryFromYear.js
@@ -0,0 +1,30 @@
+// The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+//
+//     Task
+// Given a year, return the century it is in.
+//
+// Examples
+// 1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+// 2000 --> 20
+
+// function century(year) {
+//     let yearArr = year.split('');
+//     if (year % 100 === 0){
+//         return yearArr[0]
+//     }
+//     else if (year < 100){
+//         return 1
+//     }else if(yearArr.length === 3){
+//         return parseInt(yearArr[0]) + 1;
+//     }else if (yearArr.length === 4) {
+//         return parseInt(year.slice(0, 2)) + 1;
+//     }
+//
+// }
+
+function century(year) {
+    return Math.ceil(year/100);
+}
+console.log(century('290'));
